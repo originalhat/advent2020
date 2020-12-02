ad82e4655c0eb94736ad5143cbfc2bc1cc36c63a
Devin Brown <devin.g.brown@gmail.com>
2020-12-02 13:13:06 -0700

    I expected something different.

--- day-2/passwords/lib/passwords.ex
+++ day-2/passwords/lib/passwords.ex
@@ -1,4 +1,13 @@
 defmodule Passwords do
+  def parse_file() do
+    {:ok, contents} = File.read("input.txt")
+
+    contents
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x -> String.split(x, " ", trim: true) end)
+    |> Enum.map(&pretty_parse(&1))
+  end
+
   def is_valid?(item) do
     item.password
     |> String.to_charlist()
@@ -8,5 +17,32 @@ defmodule Passwords do
 
   def in_bounds(actual, min, max) do
     actual >= min and actual <= max
+  end
+
+  def pretty_parse(x) do
+    %{
+      :min => min_count(Enum.at(x, 0)),
+      :max => max_count(Enum.at(x, 0)),
+      :character => character(Enum.at(x, 1)),
+      :password => Enum.at(x, 2)
+    }
+  end
+
+  defp min_count(x) do
+    x
+    |> String.split("-")
+    |> Enum.at(0)
+    |> String.to_integer()
+  end
+
+  defp max_count(x) do
+    x
+    |> String.split("-")
+    |> Enum.at(1)
+    |> String.to_integer()
+  end
+
+  defp character(x) do
+    x |> String.split(":") |> Enum.at(0) |> to_charlist() |> hd
   end
 end
--- day-2/passwords/test/passwords_test.exs
+++ day-2/passwords/test/passwords_test.exs
@@ -4,24 +4,34 @@ defmodule PasswordsTest do
 
   test "validates a password against it's policy" do
     assert Passwords.is_valid?(%{
-             :character => ?a,
+             :character => 97,
              :min => 1,
              :max => 3,
              :password => "abcde"
            }) == true
 
     assert Passwords.is_valid?(%{
-             :character => ?b,
+             :character => 98,
              :min => 1,
              :max => 3,
              :password => "cdefg"
            }) == false
 
     assert Passwords.is_valid?(%{
-             :character => ?c,
+             :character => 99,
              :min => 2,
              :max => 9,
              :password => "ccccccccc"
            }) == true
+  end
+
+  test "pretty parse" do
+    assert Passwords.pretty_parse(["1-7", "l:", "clllllzlllllll"]) ==
+             %{
+               :min => 1,
+               :max => 7,
+               :character => 108,
+               :password => "clllllzlllllll"
+             }
   end
 end
