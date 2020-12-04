60560e9599e39dd5aef431e839d76f5bafe755c8
Devin Brown <devin.g.brown@gmail.com>
2020-12-04 08:56:39 -0700

    Only Tom Cruise knows why.

--- day-4/passport/lib/passport.ex
+++ day-4/passport/lib/passport.ex
@@ -1,18 +1,27 @@
 defmodule Passport do
-  @moduledoc """
-  Documentation for `Passport`.
-  """
+  def parse_file() do
+    {:ok, contents} = File.read("input.txt")
 
-  @doc """
-  Hello world.
+    contents
+    |> String.split(~r{\n\s}, trim: true)
+    |> Enum.map(&String.replace(&1, "\n", " "))
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.map(
+      &Enum.into(&1, %{}, fn x ->
+        [k, v] = String.split(x, ":")
+        {String.to_atom(k), v}
+      end)
+    )
+  end
 
-  ## Examples
+  def is_valid?(passport) do
+    [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid]
+    |> Enum.map(&Map.has_key?(passport, &1))
+    |> Enum.reduce(fn x, acc -> x and acc end)
+  end
 
-      iex> Passport.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def num_valid_passports(passports) do
+    passports
+    |> Enum.count(&is_valid?/1)
   end
 end
--- day-4/passport/test/passport_test.exs
+++ day-4/passport/test/passport_test.exs
@@ -2,7 +2,55 @@ defmodule PassportTest do
   use ExUnit.Case
   doctest Passport
 
-  test "greets the world" do
-    assert Passport.hello() == :world
+  test "invalid passport" do
+    valid_passport = %{
+      :byr => "1988",
+      :iyr => "2020",
+      :eyr => "2030",
+      :hgt => "180",
+      :ecl => "brown",
+      :pid => "123456",
+      :cid => "1"
+    }
+
+    assert Passport.is_valid?(Map.delete(valid_passport, :byr)) == false
+    assert Passport.is_valid?(Map.delete(valid_passport, :iyr)) == false
+    assert Passport.is_valid?(Map.delete(valid_passport, :eyr)) == false
+    assert Passport.is_valid?(Map.delete(valid_passport, :hgt)) == false
+    assert Passport.is_valid?(Map.delete(valid_passport, :ecl)) == false
+    assert Passport.is_valid?(Map.delete(valid_passport, :pid)) == false
+  end
+
+  test "valid passport" do
+    valid_passport = %{
+      :byr => "1988",
+      :iyr => "2020",
+      :eyr => "2030",
+      :hgt => "180",
+      :ecl => "brown",
+      :pid => "123456",
+      :cid => "1"
+    }
+
+    assert Passport.is_valid?(valid_passport) == true
+  end
+
+  test "country ID can be missing if all other fields are present" do
+    north_pole_passport = %{
+      :byr => "1988",
+      :iyr => "2020",
+      :eyr => "2030",
+      :hgt => "180",
+      :ecl => "brown",
+      :pid => "123456"
+    }
+
+    assert Passport.is_valid?(north_pole_passport) == true
+  end
+
+  test "part #1" do
+    passports = Passport.parse_file()
+
+    assert Passport.num_valid_passports(passports) == 236
   end
 end
