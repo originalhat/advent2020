acf260ffe257d2c460d63e1b3ca2952c3d8b1d58
Devin Brown <devin.g.brown@gmail.com>
2020-12-05 10:57:57 -0700

    happy monday _ bleh _

--- day-5/binary/lib/binary.ex
+++ day-5/binary/lib/binary.ex
@@ -1,18 +1,35 @@
 defmodule Binary do
-  @moduledoc """
-  Documentation for `Binary`.
-  """
+  def split_code(code), do: code |> String.split_at(7)
 
-  @doc """
-  Hello world.
+  def to_b10(bin) do
+    bin
+    |> String.replace(~r/F|L/, "0")
+    |> String.replace(~r/B|R/, "1")
+    |> Integer.parse(2)
+    |> elem(0)
+  end
 
-  ## Examples
+  def seat_id(seat) do
+    Enum.at(seat, 0) * 8 + Enum.at(seat, 1)
+  end
 
-      iex> Binary.hello()
-      :world
+  def to_seat_id(boarding_passe) do
+    seat_id([to_b10(elem(boarding_passe, 0)), to_b10(elem(boarding_passe, 1))])
+  end
 
-  """
-  def hello do
-    :world
+  def load_file() do
+    {:ok, contents} = File.read("input.txt")
+
+    contents
+    |> String.split(~r/\n/, trim: true)
+  end
+
+  def find_highest() do
+    boarding_passes = Binary.load_file()
+
+    boarding_passes
+    |> Enum.map(&Binary.split_code(&1))
+    |> Enum.map(&Binary.to_seat_id(&1))
+    |> Enum.max()
   end
 end
--- day-5/binary/test/binary_test.exs
+++ day-5/binary/test/binary_test.exs
@@ -2,7 +2,33 @@ defmodule BinaryTest do
   use ExUnit.Case
   doctest Binary
 
-  test "greets the world" do
-    assert Binary.hello() == :world
+  test "splits row and seat" do
+    assert Binary.split_code("BFFFFBBRLL") == {"BFFFFBB", "RLL"}
+  end
+
+  test "converts to row number" do
+    assert Binary.to_b10("BFFFBBF") == 70
+    assert Binary.to_b10("FFFBBBF") == 14
+    assert Binary.to_b10("BBFFBBF") == 102
+  end
+
+  test "converts to seat number" do
+    assert Binary.to_b10("RRR") == 7
+    assert Binary.to_b10("RRR") == 7
+    assert Binary.to_b10("RLL") == 4
+  end
+
+  test "convert to seat ID" do
+    assert Binary.seat_id([70, 7]) == 567
+    assert Binary.seat_id([14, 7]) == 119
+    assert Binary.seat_id([102, 4]) == 820
+  end
+
+  test "converts a boarding passe to seat ID" do
+    assert Binary.to_seat_id({"BFFFBBF", "RRR"}) == 567
+  end
+
+  test "finds the highest seat ID in the file data" do
+    assert Binary.find_highest() == 998
   end
 end
