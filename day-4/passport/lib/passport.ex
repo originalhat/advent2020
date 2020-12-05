d4e09f8047ede36702403181e12b00d977c35adc
Devin Brown <devin.g.brown@gmail.com>
2020-12-05 09:37:41 -0700

    Corrected mistakes

--- day-4/passport/lib/passport.ex
+++ day-4/passport/lib/passport.ex
@@ -16,7 +16,19 @@ defmodule Passport do
 
   def is_valid?(passport) do
     [:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid]
-    |> Enum.map(&Map.has_key?(passport, &1))
+    # |> Enum.map(&Map.has_key?(passport, &1))
+    |> Enum.map(fn x ->
+      case x do
+        :byr -> passport[:byr] != nil and valid_byr?(String.to_integer(passport[:byr]))
+        :iyr -> passport[:iyr] != nil and valid_iyr?(String.to_integer(passport[:iyr]))
+        :eyr -> passport[:eyr] != nil and valid_eyr?(String.to_integer(passport[:eyr]))
+        :hgt -> passport[:hgt] != nil and valid_hgt?(passport[:hgt])
+        :hcl -> passport[:hcl] != nil and valid_hcl?(passport[:hcl])
+        :ecl -> passport[:ecl] != nil and valid_ecl?(passport[:ecl])
+        :pid -> passport[:pid] != nil and valid_pid?(passport[:pid])
+        _ -> false
+      end
+    end)
     |> Enum.reduce(fn x, acc -> x and acc end)
   end
 
@@ -24,4 +36,38 @@ defmodule Passport do
     passports
     |> Enum.count(&is_valid?/1)
   end
+
+  def valid_byr?(byr), do: byr >= 1920 and byr <= 2002
+
+  def valid_iyr?(iyr), do: iyr >= 2010 and iyr <= 2020
+
+  def valid_eyr?(eyr), do: eyr >= 2020 and eyr <= 2030
+
+  def valid_hgt?(hgt) do
+    val = String.to_integer(Enum.at(String.split(hgt, ~r/\D+/, trim: true), 0))
+
+    case Enum.at(String.split(hgt, ~r/\d+/, trim: true), 0) do
+      "cm" ->
+        val >= 150 and val <= 193
+
+      "in" ->
+        val >= 59 and val <= 76
+
+      _ ->
+        false
+    end
+  end
+
+  def valid_hcl?(hcl), do: Regex.match?(~r/#([A-Fa-f0-9]{6})/, hcl)
+
+  def valid_ecl?("amb"), do: true
+  def valid_ecl?("blu"), do: true
+  def valid_ecl?("brn"), do: true
+  def valid_ecl?("gry"), do: true
+  def valid_ecl?("grn"), do: true
+  def valid_ecl?("hzl"), do: true
+  def valid_ecl?("oth"), do: true
+  def valid_ecl?(_), do: false
+
+  def valid_pid?(pid), do: Regex.match?(~r/^\d{9}$/, pid)
 end
--- day-4/passport/test/passport_test.exs
+++ day-4/passport/test/passport_test.exs
@@ -7,9 +7,10 @@ defmodule PassportTest do
       :byr => "1988",
       :iyr => "2020",
       :eyr => "2030",
-      :hgt => "180",
-      :ecl => "brown",
-      :pid => "123456",
+      :hgt => "180cm",
+      :hcl => "#ffffff",
+      :ecl => "brn",
+      :pid => "000000999",
       :cid => "1"
     }
 
@@ -26,9 +27,10 @@ defmodule PassportTest do
       :byr => "1988",
       :iyr => "2020",
       :eyr => "2030",
-      :hgt => "180",
-      :ecl => "brown",
-      :pid => "123456",
+      :hgt => "180cm",
+      :hcl => "#ffffff",
+      :ecl => "brn",
+      :pid => "000000999",
       :cid => "1"
     }
 
@@ -40,17 +42,56 @@ defmodule PassportTest do
       :byr => "1988",
       :iyr => "2020",
       :eyr => "2030",
-      :hgt => "180",
-      :ecl => "brown",
-      :pid => "123456"
+      :hgt => "180cm",
+      :hcl => "#ffffff",
+      :ecl => "brn",
+      :pid => "000000999"
     }
 
     assert Passport.is_valid?(north_pole_passport) == true
   end
 
-  test "part #1" do
+  test "solution" do
     passports = Passport.parse_file()
 
-    assert Passport.num_valid_passports(passports) == 236
+    assert Passport.num_valid_passports(passports) == 115
+  end
+
+  test "valid birth year" do
+    assert Passport.valid_byr?(123) == false
+    assert Passport.valid_byr?(1919) == false
+    assert Passport.valid_byr?(1920) == true
+    assert Passport.valid_byr?(2002) == true
+    assert Passport.valid_byr?(2003) == false
+    assert Passport.valid_byr?(1988) == true
+    assert Passport.valid_byr?(2) == false
+  end
+
+  test "valid height" do
+    assert Passport.valid_hgt?("123cm") == false
+    assert Passport.valid_hgt?("149cm") == false
+    assert Passport.valid_hgt?("150cm") == true
+    assert Passport.valid_hgt?("193cm") == true
+    assert Passport.valid_hgt?("194cm") == false
+
+    assert Passport.valid_hgt?("57in") == false
+    assert Passport.valid_hgt?("58in") == false
+    assert Passport.valid_hgt?("59in") == true
+    assert Passport.valid_hgt?("76in") == true
+    assert Passport.valid_hgt?("77in") == false
+  end
+
+  test "valid hair color" do
+    assert Passport.valid_hcl?("#fff000") == true
+    assert Passport.valid_hcl?("fff000") == false
+    assert Passport.valid_hcl?("#fff***") == false
+    assert Passport.valid_hcl?("7c63aa") == false
+    assert Passport.valid_hcl?("z") == false
+  end
+
+  test "valid eye color" do
+    assert Passport.valid_ecl?("amb") == true
+    assert Passport.valid_ecl?("brn") == true
+    assert Passport.valid_ecl?("ylw") == false
   end
 end
